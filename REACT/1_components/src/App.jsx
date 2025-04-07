b724672c716471f8f30a838a0f1659836da76532
Leandro Sérgio <137326297+LeoSergio@users.noreply.github.com>
2025-04-07 11:19:29 -0300

    props

--- REACT/1_components/src/App.jsx
+++ REACT/1_components/src/App.jsx
@@ -1,39 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React from 'react'
 import './App.css'
 import TextComponent from './components/TextComponents'
 import { SecondText, ThirdText } from './components/OtherComponents'
 // <> </>= fragment, sera usada semppre quando agente não quer um div dentro de outra
 function App() {
-  const [count, setCount] = useState(0)
-
+//Props
   return (
     <> 
-      <div>
-        <TextComponent/>
+        <TextComponent text="Meu primmeiro Texto" description = "Descrição do primeiro Texto" />
+        <TextComponent text="Meu Segundo Texto" description = "Descrição do segundo Texto" />
+        <TextComponent text="Meu terceiro Texto" description = "Descrição do terceiro Texto" />
         <SecondText/>
         <ThirdText/>
         <TextComponent/>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Alterando components App</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
