ba1bcacf38851625ccbe8e9c1e53a607a484c39c
Leandro Sérgio <137326297+LeoSergio@users.noreply.github.com>
2025-04-07 11:06:43 -0300

    import and export

--- REACT/1_components/src/App.jsx
+++ REACT/1_components/src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import TextComponent from './components/TextComponents'
+import { SecondText, ThirdText } from './components/OtherComponents'
 // <> </>= fragment, sera usada semppre quando agente não quer um div dentro de outra
 function App() {
   const [count, setCount] = useState(0)
@@ -10,6 +11,9 @@ function App() {
   return (
     <> 
       <div>
+        <TextComponent/>
+        <SecondText/>
+        <ThirdText/>
         <TextComponent/>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -38,3 +42,4 @@ export default App
 
 
 //jsx é basicammente a mistura de javascript com html
+// components facilita na reutilização das function
---
+++ REACT/1_components/src/components/ComponentExample.jsx
@@ -0,0 +1,25 @@
+// comandos que fazem a estrutura dos components
+/*
+rafce
+import React from 'react'
+
+const ComponentExample = () => {
+  return (
+    <div>ComponentExample</div>
+  )
+}
+
+export default ComponentExample
+*/
+
+/*
+rafc
+
+import React from 'react'
+
+export const ComponentExample = () => {
+  return (
+    <div>ComponentExample</div>
+  )
+}
+*/
