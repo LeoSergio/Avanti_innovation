2a2f873d5a2e0f071af3a643a0855c835bb70256
Leandro Sérgio <137326297+LeoSergio@users.noreply.github.com>
2025-04-09 09:51:16 -0300

    create html

---
+++ REACT/2_todolist/src/components/TodoList.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import styles from './TodoList.module.css'
+
+const TodoList = () => {
+  return (
+    <div className={styles.container}>
+        <h2 className={styles.title}>Lista de Tarefas</h2>
+        <div className={styles.inputContainer}>
+            <input
+              type="text"  
+              placeholder='Adicione uma tarefa'
+              className={styles.input}
+            
+            />
+            <button className={styles.button}>Adicionar</button>
+            
+        </div>
+        <ul className={styles.taskList}>
+            <li className={styles.taskItem}>Tarefas</li>
+        </ul>
+    </div>
+  )
+}
+
+export default TodoList
